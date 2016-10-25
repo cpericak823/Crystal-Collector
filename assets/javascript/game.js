50721414a8e7c314771dd555bc61bca64fdeac1d
Cassie <cassie.pericak@northwestern.edu>
2016-10-25 14:36:42 -0500

    added pseudo code to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Start the game
+//Set score to zero
+//this will be variable called userscore and will be displayed in the ID score
+//Generate a number between 19 - 120
+//This will be a variable called random number
+//Display this number in the div ID called random-number
+//On click of each button, generate a random number between 1-12
+//This will be variable called red-jewel/blue-jewel/green-jewel/yellow-jewel
+//add this number to the user score
+//if userscore is equal to random-number, alert winner, add one to variable wins and display in stats, and reset score and random number
+//if userscore is greater than random-number, alert you lose, add one to variable losses and display in stats, and reset score and random number
