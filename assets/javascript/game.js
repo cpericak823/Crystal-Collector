8de14bba3b8bb48551fd32cedc79030e4bd0b8fd
Cassie <cassie.pericak@northwestern.edu>
2016-10-25 18:50:32 -0500

    linked to jquery and generates a random number score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,24 +7,32 @@ var userscore;
 
 
 //Generate a number between 19 - 120
-//This will be a variable called random number
+//This will be a variable called number
 //Display this number in the div ID called computer-number
 //use jquery to get element by ID and replace ID
 //use math.floor(math.random)*random-number.length to generate number
-var random - number = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-    42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-    71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
-    100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120
-]
+function chosenNumber() {
+    var number = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+        42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+        71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+        100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120
+    ];
 
+    var randomNumber = number[Math.floor(Math.random() * number.length)];
 
+    console.log(randomNumber);
+    return randomNumber;
+}
+var selectedNumber = chosenNumber();
+
+$("#computerNumberDiv").html("Computer Number: " + selectedNumber);
 
 //generate a random number between 1-12 for each jewel
 //This will be variable called red-jewel/blue-jewel/green-jewel/yellow-jewel
-var red - jewel;
-var blue - jewel;
-var green - jewel;
-var yellow - jewel;
+var redJewel;
+var blueJewel;
+var greenJewel;
+var yellowJewel;
 
 
 
